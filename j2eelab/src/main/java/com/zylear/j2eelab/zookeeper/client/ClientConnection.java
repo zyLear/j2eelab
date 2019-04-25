1c6faa2a09dcafe57418e7146dedf8a6e2886c8b
zyLear <284449710@qq.com>
2019-04-25 19:53:39 +0800

    's'

--- j2eelab/src/main/java/com/zylear/j2eelab/zookeeper/client/ClientConnection.java
+++ j2eelab/src/main/java/com/zylear/j2eelab/zookeeper/client/ClientConnection.java
@@ -13,7 +13,6 @@ public class ClientConnection {
 
     public static void main(String[] args) throws Exception {
         ZooKeeper zooKeeper = new ZooKeeper("127.0.0.1:2181", 5000, new MyWatcher());
-        zooKeeper.
 
         System.out.println(zooKeeper.getState());
         while (true) {
